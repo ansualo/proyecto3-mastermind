d16ef0153883571adbe966361d52218e14cc3aff
Andrea <andrea.sualo@gmail.com>
2023-05-19 14:11:37 +0200

    feat: create the colour picker function

--- js/main.js
+++ js/main.js
@@ -7,41 +7,8 @@ const guardadoNombre = () => {
 }
 
 // Coger el nombre, todavia no esta bien
-
 // let resultado = document.getElementById("el id del ganador");
-
 // resultado.innerHTML = `Hola ${sessionStorage.getItem("name")}`;
-
-
-
-// COLOR PICKER no funciona
-
-window.addEventListener("load", startup, false);
-
-let colorPicker = document.getElementsByClassName("colorpicker");
-let arrayColorPicker = Array.from(colorPicker);
-
-function startup(event) {
-    arrayColorPicker.map(
-        elemento => {
-            colorPicker.value = "#8a2be2";
-            colorPicker.addEventListener("input", updateFirst, false);
-            colorPicker.addEventListener("change", updateAll, false);
-            colorPicker.select();
-        }
-    )
-}
-
-function updateFirst(event) {
-    const colorSquare = document.getElementById("square1");
-    colorSquare.style.backgroundColor = event.target.value;
-}
-
-function updateAll(event) {
-    const colorSquare = document.getElementById("square1");
-    colorSquare.style.backgroundColor = event.target.value;
-}
-
 
 
 // HIDDEN ROWS 
@@ -60,10 +27,6 @@ const saveLevelAdvanced = () => {
     window.location.href = "./colores.html";
 }
 // show the row shows the correct row depending on the level
-
-// document.getElementById("beginnerRow").style.display= "none";
-// document.getElementById("intermediateRow").style.display= "none";
-
 window.onload = (event) => {
     let selectedLevel = sessionStorage.getItem("level");
     let selected = document.getElementById(selectedLevel);
@@ -72,9 +35,32 @@ window.onload = (event) => {
 
 
 
+// COLOR PICKER
 
+window.addEventListener("load", startup, false);
 
+let colorPicker = document.getElementsByClassName("colorpicker");
+let arrayColorPicker = Array.from(colorPicker);
 
+function startup(event) {
+    arrayColorPicker.map(
+        (element) => {
+            element.value = "#8a2be2";
+            element.addEventListener("input", (event)=> updateFirst(event, element), false);
+            element.addEventListener("change", (event)=> updateAll(event, element), false);
+            element.select();
+        }
+    )
+}
 
+function updateFirst(event,element) {
 
+    let colorSquare = document.getElementById(`square${element.id}`);
+    colorSquare.style.backgroundColor = event.target.value;
+}
 
+function updateAll(event,element) {
+
+    const colorSquare1 = document.getElementById(`square${element.id}`);
+    colorSquare1.style.backgroundColor = event.target.value;
+}
