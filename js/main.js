d7bf5dd45acce2466e29c855a4f1f20cfedaeb46
Andrea <andrea.sualo@gmail.com>
2023-05-20 11:38:06 +0200

    feat: create function to add rows to the game page

--- js/main.js
+++ js/main.js
@@ -28,9 +28,11 @@ const saveLevelAdvanced = () => {
 }
 
 // shows the correct row depending on the level
-window.onload = (event) => {
-    let selectedLevel = sessionStorage.getItem("level");
+
+let selectedLevel = sessionStorage.getItem("level");
     let selected = document.getElementById(selectedLevel);
+
+window.onload = (event) => {
     selected.style.display = "flex";
 };
 
@@ -48,13 +50,13 @@ function startup(event) {
     arrayColorPicker.map(
         (element) => {
             element.value = "#8a2be2";
-            element.addEventListener("input", (event)=> updateFirst(event, element), false);
+            element.addEventListener("input", (event) => updateFirst(event, element), false);
             element.select();
         }
     )
 }
 
-const updateFirst = (event,element) => {
+const updateFirst = (event, element) => {
     let colorSquare = document.getElementById(`square${element.id}`);
     colorSquare.style.backgroundColor = event.target.value;
     let color = getComputedStyle(colorSquare).backgroundColor;
@@ -68,4 +70,38 @@ const saveChosenColours = () => {
     window.location.href = "./juego.html";
 }
 
+
+// GAME
+
+window.addEventListener("load", ()=>createRows());
+
+let board = document.getElementById("game");
+
+const createRows = () => {
+
+    let mainCol = document.createElement("div");
+    mainCol.className = "col-12 d-flex";
+
+    let squaresDiv = document.createElement("div");
+    squaresDiv.className = "d-flex justify-content-evenly";
+
+    for(let i=0; i < 4; i++){
+        let eachSquare = document.createElement("div");
+        eachSquare.className = "squareGame m-2";
+        squaresDiv.appendChild(eachSquare)
+    }
+
+    let circlesDiv = document.createElement("div");
+    circlesDiv.className = "d-flex justify-content-evenly align-items-center";
+
+    for(let i=0; i < 4; i++){
+        let eachCircle = document.createElement("div");
+        eachCircle.className = "circle m-1";
+        circlesDiv.appendChild(eachCircle)
+    }
+
+    mainCol.appendChild(squaresDiv);
+    mainCol.appendChild(circlesDiv);
+    board.appendChild(mainCol);
+};
 
